6bb1c09a67932c19221922b6918d1e87bfe4cc7e
Michal Vavřík <michal.vavrik@aol.com>
2026-03-15 15:49:35 +0100

    wip
    Signed-off-by: Michal Vavřík <michal.vavrik@aol.com>

--- integration/client-cli/admin-cli/src/main/java/org/keycloak/client/cli/util/HttpUtil.java
+++ integration/client-cli/admin-cli/src/main/java/org/keycloak/client/cli/util/HttpUtil.java
@@ -43,6 +43,7 @@ import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpHead;
 import org.apache.http.client.methods.HttpOptions;
+import org.apache.http.client.methods.HttpPatch;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.methods.HttpRequestBase;
@@ -134,6 +135,9 @@ public class HttpUtil {
                 break;
             case "delete":
                 req = new HttpDelete(url);
+                break;
+            case "patch":
+                req = new HttpPatch(url);
                 break;
             case "options":
                 req = new HttpOptions(url);
