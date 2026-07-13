d5e9092f5542d335a151a0d675eff330d07c59f0
Michal Vavřík <dev@michalvavrik.net>
2026-07-14 00:31:18 +0200

    jackson-3-module
    Signed-off-by: Michal Vavřík <dev@michalvavrik.net>

---
+++ integration/admin-client-jackson3/src/main/java/org/keycloak/admin/client/jackson3/JacksonProvider3.java
@@ -0,0 +1,28 @@
+package org.keycloak.admin.client.jackson3;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import dev.resteasy.providers.jackson.ResteasyJacksonProvider;
+import tools.jackson.databind.AnnotationIntrospector;
+import tools.jackson.databind.DeserializationFeature;
+import tools.jackson.databind.json.JsonMapper;
+
+public class JacksonProvider3 extends ResteasyJacksonProvider {
+
+    private static final JsonMapper MAPPER;
+
+    static {
+        JsonMapper defaultMapper = JsonMapper.builder()
+                .changeDefaultPropertyInclusion(v -> JsonInclude.Value.construct(JsonInclude.Include.NON_NULL, JsonInclude.Include.NON_NULL))
+                .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                .build();
+        MAPPER = defaultMapper.rebuild()
+                .annotationIntrospector(AnnotationIntrospector.pair(
+                        new KeycloakAnnotationIntrospector3(),
+                        defaultMapper.serializationConfig().getAnnotationIntrospector()))
+                .build();
+    }
+
+    public JacksonProvider3() {
+        setMapper(MAPPER);
+    }
+}
