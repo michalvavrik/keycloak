bcf3575922b793f9098be78ebc03e95acbbde9c3
Michal Vavřík <dev@michalvavrik.net>
2026-09-02 22:24:33 +0200

    wip

--- services/src/main/java/org/keycloak/truststore/TruststoreReloadListener.java
+++ services/src/main/java/org/keycloak/truststore/TruststoreReloadListener.java
@@ -1,7 +1,12 @@
 package org.keycloak.truststore;
 
 import org.keycloak.models.KeycloakSession;
+import org.keycloak.provider.ProviderFactory;
 
+/**
+ * {@link ProviderFactory} that are using the system truststore should implement this interface
+ * in order to stay up to date.
+ */
 public interface TruststoreReloadListener {
 
     void truststoreReloaded(KeycloakSession session);
